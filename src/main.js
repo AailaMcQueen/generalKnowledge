f34432b6fc42705bc581420c7639676ee9c96ced
theclassicwizard <srkjadara@gmail.com>
2020-05-31 14:57:07 +0530

    creatingTemplate

--- src/main.js
+++ src/main.js
@@ -1,17 +1,28 @@
-var url = "https://opentdb.com/api.php?amount=1&type=multiple"
+var url = "https://opentdb.com/api.php?amount=1&type=multiple";
+var buttonRefresh = document.querySelector(".buttonRefresh");
+var questionArray = [];
+var questionDiv = document.querySelectorAll(".questionCard");
 
-
-fetch(url)
-.then(function(request){
-    if(!request.ok){
-        throw Error(request.status);
+buttonRefresh.addEventListener("click", function(){
+    questionArray.splice(0, questionArray.length);
+    for(var i=0;i<7;i++){
+        fetch(url)
+        .then(function(request){
+            if(!request.ok){
+                throw Error(request.status);
+            }
+            return request.json();
+        })
+        .then(function(response){
+            var data = response.results[0];
+            questionArray.push(data);
+        })
+        .catch(function(error){
+            alert(error);
+        });
     }
-    return request.json();
-})
-.then(function(response){
-    var data = response.results[0];
-    return data;
-})
-.catch(function(error){
-    alert(error);
 });
+
+function questionUpdate(question){
+    console.log("Hello");
+}
