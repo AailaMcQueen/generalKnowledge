70b0f617c455f7b53af2ccac3fc271107f69a73f
theclassicwizard <srkjadara@gmail.com>
2020-06-04 12:08:45 +0530

    timerUpdate

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ var url = "https://opentdb.com/api.php?amount=1&type=multiple";
 var buttonRefresh = document.querySelector(".buttonRefresh");
 var questionArray = [];
 var questionDiv = document.querySelectorAll(".questionCard");
+var timerX = document.querySelector(".seconds");
+var timerY = document.querySelector(".timer");
+var seconds = 45;
 
 buttonRefresh.addEventListener("click", function(){
     questionArray.splice(0, questionArray.length);
@@ -25,4 +28,22 @@ buttonRefresh.addEventListener("click", function(){
 
 function questionUpdate(question){
     console.log("Hello");
+}
+
+var clockTimer = setInterval(timerFunction, 1000);
+
+function timerFunction(){
+    seconds--;
+    var x = "0";
+    if(seconds>=10){
+        timerX.innerHTML = seconds;
+    }
+    else{
+        timerY.style.webkitAnimationPlayState = "running";
+        timerX.innerHTML = x + seconds;
+    }
+    if(seconds==0){
+        timerY.style.webkitAnimationPlayState = "paused";
+        clearInterval(clockTimer);
+    }
 }
